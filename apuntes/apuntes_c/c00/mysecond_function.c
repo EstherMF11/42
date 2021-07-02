07d7722f14bd66072c57b4ba45dd14f1c085dd1b
Esther Molinero fernandez <emoliner@c2r4s3.42madrid.com>
2021-07-02 09:11:51 +0200

    apuntes again

---
+++ apuntes/apuntes_c/c00/mysecond_function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float	fct(int a, float x);
+
+int	main(void)
+{
+	ftc(42, 0.5);
+	printf("%f\n", a);
+	printf("%f\n", x);
+	return (0);
+}
+
+float	ftc(int a, float x)
+{
+	int	a;
+	float	x;
+
+	printf("Coucou 42\n");
+	return (a * x);
+}
---
+++ apuntes/apuntes_c/c01/null_ptr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	*ptr;
+
+	ptr = 0;
+    return (0);
+}
+
+/**
+ *  null: "vacio" 
+ * Por convenio si un puntero tiene un valor 0, no apunta a ningún sitio.
+ * No tiene dirección real a la que ir.
+ * Aplicación: Si tenemos una serie de punteros podemos saber
+ * cual va hacia un elemento y cuales no tienen una dirección a la que ir.
+*/
