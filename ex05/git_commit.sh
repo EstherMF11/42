2ca5a361d27d1f852fbe047fb9f470242876cf4a
Esther Molinero fernandez <emoliner@c2r4s1.42madrid.com>
2021-07-08 12:51:41 +0200

    delete extra folders

--- ex05/git_commit.sh
+++
@@ -1,2 +0,0 @@
-git log --pretty=%H -5
-#Devuelve los ultimos 5 commit realizados
--- ex06/git_ignore.sh
+++
@@ -1,2 +0,0 @@
-git ls-files --ignored --exclude-standard --others
-#Devuelve una lista de archivos ignorados en cualquier directorio y subdirectorio
